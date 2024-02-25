e24109ed09b326c42ece6c14c98d6d3a19f88f58
CharlieAgoge <154438350+CharlieAgoge@users.noreply.github.com>
2024-02-25 09:14:03 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-2" # London region
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-NAME"
+}
+resource "aws_s3_bucket" "terraform_today" {
+  bucket = "terraform-up-and-running-state-today"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state-charlie"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
+resource "aws_instance" "test5" {
+  ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+  instance_type = "t2.micro"  # Change to the desired instance type
+  tags = {
+    Name = "sundayexample-instance"
+}
+}
+resource "aws_s3_bucket" "now" {
+  bucket = "now1"
+}
