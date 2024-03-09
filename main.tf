70371d32d43132a188b6aeacb9d94eec6b8c54f5
“Charlie” <charlie.banyard@cyberagoge.comclear>
2024-03-09 11:53:40 +0000

    testing

--- main.tf
+++ main.tf
@@ -25,6 +25,6 @@ resource "aws_instance" "test5" {
   ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
   instance_type = "t2.micro"  # Change to the desired instance type
   tags = {
-    Name = "sundayexample-instance"
+    Name = "sundayexample-instance" 
 }
 }
